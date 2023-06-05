7cb88f893185a14d1b4c5b995d707c9373137729
quyquang1809 <accriseofking2@gmail.com>
2023-06-05 08:45:36 +0700

    build

--- src/assets/images/index.js
+++ src/assets/images/index.js
@@ -1,7 +1,6 @@
 const images = {
     logo: require('~/assets/images/logocty.jpg'),
     noImage: require('~/assets/images/error-Image.png'),
-    anhdep: require('~/assets/images/anhdep.jpg'),
     anhJerry: require('~/assets/images/jerry.png'),
     sliderHomePage1: require('~/assets/images/slider-home1.jpg'),
     sliderHomePage2: require('~/assets/images/slider-home2.jpg'),
